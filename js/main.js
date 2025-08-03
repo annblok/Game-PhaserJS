72d963e94950bae3cf5c0e57435eb7b8515e6035
Administrator <Anya14_93@mail.ru>
2025-08-03 18:01:30 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-import PreloadScene from 'scenes/PreloadScene.js';
-import MenuScene from 'scenes/MenuScene.js';
-import LevelScene1 from 'scenes/LevelScene1.js';
-import LevelScene2 from 'scenes/LevelScene2.js';
-import WinScene from 'scenes/WinScene.js';
-import GameOverScene from 'scenes/GameOverScene.js';
+import PreloadScene from 'https://annblok.github.io/Game-PhaserJS/scenes/PreloadScene.js';
+import MenuScene from 'https://annblok.github.io/Game-PhaserJS/scenes/MenuScene.js';
+import LevelScene1 from 'https://annblok.github.io/Game-PhaserJS/scenes/LevelScene1.js';
+import LevelScene2 from 'https://annblok.github.io/Game-PhaserJS/scenes/LevelScene2.js';
+import WinScene from 'https://annblok.github.io/Game-PhaserJS/scenes/WinScene.js';
+import GameOverScene from 'https://annblok.github.io/Game-PhaserJS/scenes/GameOverScene.js';
 
 const config = {
     type: Phaser.AUTO,
